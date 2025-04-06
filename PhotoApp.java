42b52761146224ebe4bf235333ac4c916e79fdb8
Toma Takamatsu <takamatsutoma@gmail.com>
2025-04-06 17:16:55 -0400

    basic
    to get it running

---
+++ PhotoApp.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class PhotoApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Click me");
+        btn.setOnAction(event -> System.out.println("Button clicked!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("Photo App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
