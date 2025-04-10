87facdf6c35e19dff02e75b3077674a2261afc14
Toma Takamatsu <takamatsutoma@gmail.com>
2025-04-10 04:12:28 -0400

    Started user interface. Also finished Tag class

--- src/app/model/Tag.java
+++ src/app/model/Tag.java
@@ -1,5 +1,42 @@
 package app.model;
 
-public class Tag {
-    
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Tag implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String value;
+
+    public Tag(String name, String value){
+        this.name = name.trim().toLowerCase();
+        this.value = value.trim().toLowerCase();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o){ // Method to check if two tags are equal
+        if (this == o) return true;
+        if (!(o instanceof Tag)) return false;
+        Tag tag = (Tag) o;
+        return name.equals(tag.getName()) && value.equals(tag.getValue());
+    }
+
+    @Override
+    public String toString(){ // Making a string converter in case we need to print
+        return name + "=" + value;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, value);
+    }
 }
--- src/view/controller/UserController.java
+++ src/view/controller/UserController.java
@@ -1,5 +1,40 @@
 package view.controller;
 
+import app.model.Album;
+import app.model.User;
+import app.util.UserManager;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+
+
 public class UserController {
+    @FXML private Label welcomeLabel;
+    @FXML private TableView<Album> albumTableView;
+    @FXML private TableColumn<Album, String> nameColumn;
+    @FXML private TableColumn<Album, Integer> countColumn;
+    @FXML private TableColumn<Album, String> rangeColumn;
+    @FXML private TextField albumNameField;
+    @FXML private Button createAlbumButton;
+    @FXML private Button deleteAlbumButton;
+    @FXML private Button renameAlbumButton;
+    @FXML private Button openAlbumButton;
+    @FXML private Button logoutButton;
+
+    private User currentUser;
+    private ObservableList<Album> allAlbums;
     
+    public void setUser(User user){
+        this.currentUser = user;
+        welcomeLabel.setText("Welcome, " + user.getUsername() + "!");
+    }
+
+    public void initialize(){
+
+    }
+
 }
